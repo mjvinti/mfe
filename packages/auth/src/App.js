29ebd3931d5d7ca8d3e02c16c8a999b7e76d4ae9
mjvinti <mjvinti@gmail.com>
2022-09-22 10:06:30 -0400

    added signin and signup forms to app

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
 const generateClassName = createGenerateClassName({ productionPrefix: 'au' });
 
 const App = ({ history }) => {
@@ -10,6 +13,8 @@ const App = ({ history }) => {
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
+                        <Route path='/auth/signin' component={Signin} />
+                        <Route path='/auth/signup' component={Signup} />
                     </Switch>
                 </Router>
             </StylesProvider>
