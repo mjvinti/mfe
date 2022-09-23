1c0dff56dc5e79e74463372b689760a79488eb32
mjvinti <mjvinti@gmail.com>
2022-09-23 17:20:47 -0400

    communicating authentication state

--- packages/container/src/components/AuthApp.js
+++ packages/container/src/components/AuthApp.js
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useEffect, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const AuthApp = () => {
+const AuthApp = ({ onSignIn }) => {
     const ref = useRef(null);
     const history = useHistory();
 
@@ -16,7 +16,7 @@ const AuthApp = () => {
                     history.push(nextPathname);
                 }
             },
-            onSignIn: () => console.log('user signed in')
+            onSignIn
         });
 
         history.listen(onParentNavigate);
